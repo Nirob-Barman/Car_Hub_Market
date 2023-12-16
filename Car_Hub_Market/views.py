7e3d1e9c3e737c19300b0d582f1306b52eb8e013
Nirob-Barman <niroborin111@gmail.com>
2023-12-16 17:02:29 +0600

    Title set to all route

--- Car_Hub_Market/views.py
+++ Car_Hub_Market/views.py
@@ -7,7 +7,9 @@ def home(request, brand_name=None):
     cars = Car.objects.all()
     brands = Brands.objects.all()
 
+    brandName = brand_name
+
     if brand_name:
         cars = cars.filter(brand__name=brand_name)
 
-    return render(request, 'home.html', {'cars': cars, 'brands': brands})
+    return render(request, 'home.html', {'cars': cars, 'brands': brands, 'brandName': brandName})
