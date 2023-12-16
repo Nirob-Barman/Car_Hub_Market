80e0d40df188dd4c5262c63084333dd3e6b9063a
Nirob-Barman <niroborin111@gmail.com>
2023-12-16 15:08:14 +0600

    Comment some unwanted topics

--- cars/admin.py
+++ cars/admin.py
@@ -4,3 +4,20 @@ from . import models
 # Register your models here.
 admin.site.register(models.Car)
 admin.site.register(models.Comment)
+
+
+# from django.contrib import admin
+# from .models import Car, Comment
+
+
+# class CarAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'brand', 'price', 'quantity', 'display_buyers')
+
+#     def display_buyers(self, obj):
+#         return ", ".join([buyer.username for buyer in obj.buyers.all()])
+
+#     display_buyers.short_description = 'Buyers'
+
+
+# admin.site.register(Car, CarAdmin)
+# admin.site.register(Comment)
--- cars/models.py
+++ cars/models.py
@@ -36,3 +36,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return 'Comment {} by {}'.format(self.body, self.name)
+
+
+
